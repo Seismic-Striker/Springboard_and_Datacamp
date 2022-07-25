defb9e09ec62c42951803402e0c2d226860e3bd9
Seismic-Striker <derek.t.sullivan88@gmail.com>
2022-07-25 09:55:11 -0600

    final capstone

---
+++ final_capstone/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final capstone project for Springboards Data Science Curriculum',
+    author='Derek Sullivan',
+    license='MIT',
+)
